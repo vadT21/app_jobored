6f9faac145543a46c72661940bc9d9817cf262c1
Vadim <vadimtomin21@yandex.by>
2023-05-10 23:59:35 +0200

    create constants
    create constants

---
+++ src/constants/links.ts
@@ -0,0 +1,18 @@
+export const JOB_SEARCH_LINK = {
+  title: "JobSearch",
+  link: "/",
+};
+
+export const JOB_DETAIL_LINK = {
+  title: "JobDetail",
+  link: "/job/:id",
+};
+export const JOB_FAVORITE_LINK = {
+  title: "JobFavorite",
+  link: "/favorite",
+};
+
+export const JOB_EMPTY_LINK = {
+  title: "JobEmpty",
+  link: "*",
+};
