6473e21e40319516a2ccaeaeac4bc7e045ce41fc
Vadim <vadimtomin21@yandex.by>
2023-05-10 23:57:51 +0200

    edit structure app
    edit structure app

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,48 @@
-import FilteringApp from "./components/filtering/FilteringApp";
+import { AppShell, Header } from "@mantine/core";
+import { useState, useEffect } from "react";
 import HeaderApp from "./components/header/HeaderApp";
-import JobCardItem from "./components/jobcard/JobCardItem";
-import JobCardList from "./components/jobcard/JobCardList";
-import JobDetailList from "./components/jobcard/JobDetailList";
-import PaginationApp from "./components/pagination/PaginationApp";
-import SearchingApp from "./components/searching/SearchingApp";
+
+import Rounting from "./routing/Rounting";
+import PasswordAuthResponse from "./API/auth";
+import { useTokenStore } from "./store";
 
 function App() {
-  const links = [
-    {
-      link: "/about",
-      label: "Features",
-    },
-    {
-      link: "/pricing",
-      label: "Pricing",
-    },
-    {
-      link: "/learn",
-      label: "Learn",
-    },
-    {
-      link: "/community",
-      label: "Community",
-    },
-  ];
-
-  const item = {
-    image:
-      "https://images.unsplash.com/photo-1602080858428-57174f9431cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    category: "technology",
-    title: "The best laptop for Frontend engineers in 2022",
-    date: "Feb 6th",
-    author: {
-      name: "Elsa Brown",
-      avatar:
-        "https://images.unsplash.com/photo-1628890923662-2cb23c2e0cfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    },
-  };
+  const addSecretToken = useTokenStore((state) => state.addSecretToken);
+  const secretToken = useTokenStore((state) => state.secretToken);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if (!secretToken) {
+          console.log("work token fetch");
+          const token = await PasswordAuthResponse();
+          addSecretToken(token);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <>
-      <HeaderApp links={links} />
-      <SearchingApp />
-      <FilteringApp />
-      <JobCardList />
-      <PaginationApp />
-      <JobDetailList />
+      <AppShell
+        padding="md"
+        header={
+          <Header height={60} p="xs">
+            <HeaderApp />
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        {<Rounting />}
+      </AppShell>
     </>
   );
 }
