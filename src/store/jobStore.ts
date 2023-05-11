1f4214d49ff62d06fa9033df094a99e4dee7e7a2
Vadim <vadimtomin21@yandex.by>
2023-05-11 13:32:15 +0200

    add new value
    add new value to store

--- src/store/jobStore.ts
+++ src/store/jobStore.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import JobRequestResponse from "../API/jobrequest";
+import DetailJobRequestResponse from "../API/detailjobrequest";
+import CatalogRequestResponse from "../API/catalogrequest";
 
 interface JobState {
   loading: boolean;
@@ -9,6 +11,19 @@ interface JobState {
   totalCountPage: number;
   jobs: T[];
   fetchJobs: (token: string, page: number) => void;
+
+  job: T | null;
+  fetchJob: (token: string, id: string | undefined) => void;
+  catalogues: Cat[];
+  fetchCatalogues: (token: string) => void;
+  keywords: string | null;
+  addKeywords: (value: string | null) => void;
+  salaryFrom: number | "";
+  addSalaryFrom: (value: number) => void;
+  salaryTo: number | "";
+  addSalaryTo: (value: number) => void;
+  catalogue: number | null;
+  addCatalogue: (value: string) => void;
 }
 
 interface T {
@@ -21,6 +36,14 @@ interface T {
   currency: string | undefined;
   favorite: boolean;
 }
+
+interface Cat {
+  key: number;
+  title: string;
+  title_rus: string;
+  title_trimmed: string;
+  url_rus: string;
+}
 export const useJobStore = create<JobState>()((set, get) => ({
   currentPage: 1,
   changeCurrentPage: (page) => set({ currentPage: page }),
@@ -29,20 +52,57 @@ export const useJobStore = create<JobState>()((set, get) => ({
   error: null,
   jobs: [],
   fetchJobs: async (token, page) => {
-    console.log("work add JR");
     set({ loading: true });
     try {
       const res = await JobRequestResponse(token, page);
       console.log(res);
-
       if (!res) throw new Error("Failed to fetch! Try again.");
-
       set({ jobs: res.objects, error: null });
     } catch (error: any) {
       set({ error: error.message });
     } finally {
+      console.log(get().catalogues);
       set({ loading: false });
-      console.log("stop JR");
     }
   },
+  job: null,
+  fetchJob: async (token, id) => {
+    set({ loading: true });
+    try {
+      const res = await DetailJobRequestResponse(token, id);
+      console.log(res);
+      if (!res) throw new Error("Failed to fetch! Try again.");
+      set({ job: res, error: null });
+    } catch (error: any) {
+      set({ error: error.message });
+    } finally {
+      console.log(get().catalogues);
+      set({ loading: false });
+    }
+  },
+  catalogues: [],
+  fetchCatalogues: async (token) => {
+    set({ loading: true });
+    try {
+      const res = await CatalogRequestResponse(token);
+      if (!res) throw new Error("Failed to fetch! Try again.");
+      set({ catalogues: res, error: null });
+    } catch (error: any) {
+      set({ error: error.message });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  keywords: null,
+  addKeywords: (value) => set({ keywords: value }),
+
+  salaryFrom: "",
+  addSalaryFrom: (value) => set({ salaryFrom: value }),
+
+  salaryTo: "",
+  addSalaryTo: (value) => set({ salaryTo: value }),
+
+  catalogue: null,
+  addCatalogue: (value) => set((state) => ({ catalogue: 1 })),
 }));
