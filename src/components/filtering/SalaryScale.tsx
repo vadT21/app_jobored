a1e66548fd1c7a56e196488545cadba9792d8079
Vadim <vadimtomin21@yandex.by>
2023-05-11 13:31:57 +0200

    create control
    create control to component

--- src/components/filtering/SalaryScale.tsx
+++ src/components/filtering/SalaryScale.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { NumberInput } from "@mantine/core";
 
 const SalaryScale = () => {
+  const [fromValue, setFromValue] = useState<number | "">();
+  const [toValue, setToValue] = useState<number | "">();
+
   return (
     <>
       <div className="Ocl">
@@ -11,16 +15,20 @@ const SalaryScale = () => {
           stepHoldDelay={500}
           stepHoldInterval={100}
           sx={{ marginBottom: 10 }}
-          step={50}
+          step={1}
           min={0}
+          value={fromValue}
+          onChange={setFromValue}
         />
         <NumberInput
           placeholder={"До"}
           stepHoldDelay={500}
-          step={50}
+          step={1}
           min={0}
           stepHoldInterval={(t) => Math.max(1000 / t ** 2, 25)}
           sx={{ marginBottom: 20 }}
+          value={toValue}
+          onChange={setToValue}
         />
       </div>
     </>
