e21d7eb791dd010a9e8b26b64b10528f34ae918b
Vadim <vadimtomin21@yandex.by>
2023-05-11 13:31:32 +0200

    give dynamic
    give dynamic to component

--- src/components/filtering/IndustryDropdown.tsx
+++ src/components/filtering/IndustryDropdown.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { Select } from "@mantine/core";
 
-const IndustryDropdown = () => {
+interface Cat {
+  key: number;
+  title: string;
+  title_rus: string;
+  title_trimmed: string;
+  url_rus: string;
+}
+interface Props {
+  catalogues: Cat[];
+}
+const IndustryDropdown = ({ catalogues }: Props) => {
+  const items = catalogues.length
+    ? catalogues.map((el) => ({
+        value: el.title,
+        label: el.title,
+        key: el.key,
+      }))
+    : [];
+
+  const handleChange = (value: string) => {
+    //тут возвращать
+    console.log(value);
+  };
+
   return (
     <Select
       label="Your favorite framework/library"
       placeholder="Pick one"
       searchable
       nothingFound="No options"
-      data={["React", "Angular", "Svelte", "Vue"]}
+      data={items}
+      onChange={handleChange}
       mt="md"
     />
   );
