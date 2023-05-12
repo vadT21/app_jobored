ee106f6a395d3e569e6fc4db6326389bb9035136
Vadim <vadimtomin21@yandex.by>
2023-05-12 14:34:03 +0200

    create logic star
    the logic of the star's work is outlined

---
+++ src/components/favoriteStar/FavoriteStar.tsx
@@ -0,0 +1,67 @@
+import { createStyles, Group, ActionIcon } from "@mantine/core";
+import { useState } from "react";
+import { useFavoritesStore } from "../../store";
+import { IconStar } from "../icons";
+
+const useStyles = createStyles((theme) => ({
+  star: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+  },
+}));
+
+interface T {
+  id: number;
+  profession: string | undefined;
+  town: { title: string | undefined };
+  type_of_work: { title: string | undefined };
+  payment_to: number | undefined;
+  payment_from: number | undefined;
+  currency: string | undefined;
+  favorite: boolean;
+}
+
+const FavoriteStar = ({
+  id,
+  type_of_work,
+  profession,
+  town,
+  payment_to,
+  payment_from,
+  currency,
+  favorite,
+}: T) => {
+  const { classes } = useStyles();
+
+  const [isActive, setIsActive] = useState(favorite);
+
+  const toggleFavoriteJobs = useFavoritesStore(
+    (state) => state.toggleFavoriteJobs,
+  );
+
+  const handleStarClick = () => {
+    console.log("work star");
+    setIsActive(!isActive);
+    toggleFavoriteJobs({
+      id,
+      type_of_work: { title: type_of_work.title },
+      profession,
+      town: { title: town.title },
+      payment_to,
+      payment_from,
+      currency,
+      favorite,
+    });
+  };
+
+  return (
+    <Group className={classes.star} onClick={handleStarClick}>
+      <ActionIcon variant="transparent" radius="md" size={36}>
+        <IconStar fill={isActive ? "#5E96FC" : "#7c2207"} />
+      </ActionIcon>
+    </Group>
+  );
+};
+
+export default FavoriteStar;
