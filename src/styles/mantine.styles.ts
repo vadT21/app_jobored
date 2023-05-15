dfab2014bd16a9a18bbfe35b7cf85e2cfd194741
Vadim <vadimtomin21@yandex.by>
2023-05-16 01:37:46 +0200

    create basic styles colors
    create basic styles colors

---
+++ src/styles/mantine.styles.ts
@@ -0,0 +1,22 @@
+import { MantineThemeOverride } from "@mantine/core";
+
+const theme: MantineThemeOverride = {
+  colors: {
+    white: ["#FFFFFF"],
+    grey100: ["#F5F5F6"],
+    grey200: ["#EAEBED"],
+    grey300: ["#D5D6DC"],
+    grey500: ["#ACADB9"],
+    grey600: ["#7B7C88"],
+    black: ["#232134"],
+    blue100: ["#DEECFF"],
+    blue200: ["#C9E0FF"],
+    blue300: ["#B7D6FF"],
+    blue400: ["#92C1FF"],
+    blue500: ["#5E96FC"],
+    blue600: ["#3B7CD3"],
+  },
+  fontFamily: "Inter",
+};
+
+export default theme;
