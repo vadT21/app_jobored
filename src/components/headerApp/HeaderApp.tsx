0f62f8e0ee5a53c709b8c8945521623e6c401dd4
Vadim <vadimtomin21@yandex.by>
2023-05-17 08:27:24 +0200

    putting navigation
    Putting navigation in a separate component

--- src/components/headerApp/HeaderApp.tsx
+++ src/components/headerApp/HeaderApp.tsx
@@ -1,47 +1,16 @@
-import { useState } from "react";
-import { Header, Container, Paper, Burger } from "@mantine/core";
-import { Link } from "react-router-dom";
-import { useDisclosure } from "@mantine/hooks";
+import { Header, Container } from "@mantine/core";
 import JoboredLogo from "../logoApp/JoboredLogo";
+import NavigationApp from "../navigationApp/NavigationApp";
 import { useStyles } from "./HeaderApp.style";
-import { ROUTE_LINKS } from "../../constants";
 
 const HeaderApp = () => {
-  const links = [ROUTE_LINKS.searchPage, ROUTE_LINKS.favoritePage];
-
-  const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
-  const { classes, cx } = useStyles();
-
-  const items = links.map((link) => (
-    <Link
-      key={link.label}
-      to={link.link}
-      className={cx(classes.link, {
-        [classes.linkActive]: active === link.link,
-      })}
-      onClick={() => {
-        setActive(link.link);
-      }}
-    >
-      {link.label}
-    </Link>
-  ));
+  const { classes } = useStyles();
 
   return (
     <Header height={60} className={classes.header}>
       <Container className={classes.wrapper}>
         <JoboredLogo />
-        <Paper component="nav" className={classes.links}>
-          {items}
-        </Paper>
-
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          className={classes.burger}
-          size="sm"
-        />
+        <NavigationApp />
       </Container>
     </Header>
   );
