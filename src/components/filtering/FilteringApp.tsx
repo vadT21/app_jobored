613f4bffa95a8e405f95725ccb1c176f5a1f5448
Vadim <vadimtomin21@yandex.by>
2023-05-18 01:05:46 +0200

    add burger menu
    add burger menu

--- src/components/filtering/FilteringApp.tsx
+++ src/components/filtering/FilteringApp.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Group, Button, Paper } from "@mantine/core";
+import { Group, Button, Paper, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import IndustryDropdown from "../industryDropdown/IndustryDropdown";
 import SalaryScale from "../salaryScale/SalaryScale";
 import { useStyles } from "./FilteringApp.style";
@@ -8,18 +9,26 @@ import HeaderFIltering from "./headerFiltering/HeaderFIltering";
 const FilteringAppC = () => {
   const { classes } = useStyles();
   console.log("render filtre");
-
+  const [opened, { toggle }] = useDisclosure(false);
   return (
-    <Paper component="section" className={classes.container}>
-      <HeaderFIltering />
+    <>
+      <Paper component="section" className={classes.container}>
+        <HeaderFIltering />
 
-      <Group className={classes.params}>
-        <IndustryDropdown />
-        <SalaryScale />
-      </Group>
+        <Group className={classes.params}>
+          <IndustryDropdown />
+          <SalaryScale />
+        </Group>
 
-      <Button className={classes.control}>Применить</Button>
-    </Paper>
+        <Button className={classes.control}>Применить</Button>
+      </Paper>
+      <Burger
+        opened={opened}
+        onClick={toggle}
+        className={classes.burger}
+        size="sm"
+      />
+    </>
   );
 };
 
