5adbac43612d8cace3ca212b3e743733fd5dc214
Vadim <vadimtomin21@yandex.by>
2023-05-17 08:26:45 +0200

    remove styles
    remove styles

--- src/components/headerApp/HeaderApp.style.ts
+++ src/components/headerApp/HeaderApp.style.ts
@@ -26,34 +26,4 @@ export const useStyles = createStyles((theme) => ({
       justifyContent: "space-around",
     },
   },
-
-  links: {
-    display: "flex",
-    padding: 0,
-    paddingTop: 2,
-    gap: 60,
-    [theme.fn.smallerThan("xs")]: {
-      display: "none",
-    },
-  },
-
-  burger: {
-    [theme.fn.largerThan("xs")]: {
-      display: "none",
-    },
-  },
-
-  link: {
-    color: theme.colors.black,
-    textDecoration: "none",
-    fontStyle: "normal",
-    fontWeight: 500,
-    fontSize: 16,
-  },
-
-  linkActive: {
-    "&, &:hover": {
-      color: theme.colors.blue500,
-    },
-  },
 }));
