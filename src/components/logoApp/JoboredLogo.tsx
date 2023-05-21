539291ea58e55ff12bb9ebca0a56b1cb07c0feca
Vadim <vadimtomin21@yandex.by>
2023-05-21 23:05:41 +0200

    add handle click
    add handle click

--- src/components/logoApp/JoboredLogo.tsx
+++ src/components/logoApp/JoboredLogo.tsx
@@ -1,11 +1,18 @@
 import { Group, Title } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 import { IconLogo } from "../icons";
 import { useStyles } from "./JoboredLogo.style";
 
 const JoboredLogo = () => {
+  // при клике переходить на главную + обновлять ее
+  const navigate = useNavigate();
+  const handleClickReloadPage = () => {
+    navigate("/jobored");
+    window.location.reload();
+  };
   const { classes } = useStyles();
   return (
-    <Group className={classes.wrapper}>
+    <Group className={classes.wrapper} onClick={handleClickReloadPage}>
       <IconLogo />
       <Title order={1} className={classes.title}>
         Jobored
