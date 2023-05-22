2b999a2ff4094dd093eb1e44adde8ea9362b21fa
Vadim <vadimtomin21@yandex.by>
2023-05-22 23:20:30 +0200

    add cursor on logo
    add cursor on logo

--- src/components/logoApp/JoboredLogo.style.ts
+++ src/components/logoApp/JoboredLogo.style.ts
@@ -2,6 +2,7 @@ import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
   wrapper: {
+    cursor: "pointer",
     background: theme.colors.white,
     display: "flex",
     alignItems: "center",
