ff8bfec72062c3ed2491d23dd3f63d3c66f42714
Vadim <vadimtomin21@yandex.by>
2023-05-22 17:46:19 +0200

    remove empty route
    remove empty route to *

--- src/routing/Rounting.tsx
+++ src/routing/Rounting.tsx
@@ -8,7 +8,6 @@ import { EmptyPage, DetailPage, SearchPage, FavoritePage } from "../pages";
 import { Layout } from "../layout/Layout";
 
 //создаем роуты pages
-// empty явл. как самостоятельной так и при любой другой ссылке который нету
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={ROUTE_LINKS.searchPage.link} element={<Layout />}>
@@ -18,7 +17,6 @@ export const router = createBrowserRouter(
         path={`${ROUTE_LINKS.detailPage.link}/:id`}
         element={<DetailPage />}
       />
-      <Route path={ROUTE_LINKS.emptyPage.link} element={<EmptyPage />} />
       <Route path="*" element={<EmptyPage />} />
     </Route>,
   ),
