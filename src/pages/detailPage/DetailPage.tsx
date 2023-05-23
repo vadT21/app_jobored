05f0cfaf105413722a175083f3e9b365ff18f94f
Vadim <vadimtomin21@yandex.by>
2023-05-23 13:35:42 +0200

    edit logic get detail
    edit logic get detail

--- src/pages/detailPage/DetailPage.tsx
+++ src/pages/detailPage/DetailPage.tsx
@@ -1,12 +1,11 @@
 import { SimpleGrid, Container } from "@mantine/core";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Navigate, useParams } from "react-router-dom";
-import { useJobStore, useTokenStore } from "../../store";
+import { useJobStore } from "../../store";
 import JobDetailInfo from "../../components/jobdetail/jobDetailInfo/JobDetailInfo";
 import JobDetailDescription from "../../components/jobdetail/jobDetailDescription/JobDetailDescription";
 //import { useStyles } from "./DetailPage.style";
 import LoaderApp from "../../components/loader/LoaderApp";
-import { JobDataI } from "../../models";
 import { ROUTE_LINKS } from "../../constants";
 
 export const DetailPage = () => {
@@ -15,24 +14,17 @@ export const DetailPage = () => {
   //получение id
   const { id } = useParams();
 
-  const token = useTokenStore((state) => state.secretToken);
+  const job = useJobStore((state) => state.jobs);
+
   const fetchJobDetail = useJobStore((state) => state.fetchJobDetail);
-
-  // контролируемый сет вакансии
-  const [job, setJob] = useState<JobDataI[]>([]);
-
-  //запрос на получение вакансии
-  async function fetchJob() {
-    const res = await fetchJobDetail(token, id);
-    if (res) {
-      setJob([res]);
-    }
-  }
+  const loadingJob = useJobStore((state) => state.loading);
 
   const errorDetail = useJobStore((state) => state.errorDetail);
 
   useEffect(() => {
-    fetchJob();
+    if (id) {
+      fetchJobDetail(id);
+    }
   }, []);
 
   // обработка ошибки
@@ -42,13 +34,17 @@ export const DetailPage = () => {
 
   return (
     <Container maw={773}>
-      {job.length ? (
-        <SimpleGrid cols={1}>
-          <JobDetailInfo {...job[0]} />
-          <JobDetailDescription value={job[0].vacancyRichText} />
-        </SimpleGrid>
-      ) : (
+      {loadingJob ? (
         <LoaderApp />
+      ) : (
+        <>
+          {job.length && (
+            <SimpleGrid cols={1}>
+              <JobDetailInfo {...job[0]} />
+              <JobDetailDescription value={job[0].vacancyRichText} />
+            </SimpleGrid>
+          )}
+        </>
       )}
     </Container>
   );
