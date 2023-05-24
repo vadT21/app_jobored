2fbe0df547331cedf800ff52716db6850a67bdbc
Vadim <vadimtomin21@yandex.by>
2023-05-24 12:28:10 +0300

    removed block try/catch
     removed block try/catch it's optional in API

--- src/API/tokenRequest.ts
+++ src/API/tokenRequest.ts
@@ -13,21 +13,18 @@ export const TokenRequest = async (): Promise<string> => {
   const client_secret = API_DATA.client_secret;
   const hr = API_DATA.hr;
   const url = `${API_DATA.base_url}/oauth2/password`;
-  try {
-    const response = await axios.get<PasswordAuthResponseI>(url, {
-      headers,
-      params: {
-        login,
-        password,
-        client_id,
-        client_secret,
-        hr,
-      },
-    });
-    const access_token = response.data.access_token;
-    return access_token;
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
+ 
+  const response = await axios.get<PasswordAuthResponseI>(url, {
+    headers,
+    params: {
+      login,
+      password,
+      client_id,
+      client_secret,
+      hr,
+    },
+  });
+  const access_token = response.data.access_token;
+  return access_token;
+  
 };
